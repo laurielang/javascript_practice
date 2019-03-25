a1d165294c07116246bd9342770069cc34137ba2
laurielang <laurielang@hotmail.co.uk>
2019-03-25 17:33:57 +0000

    middleCharacter update

--- challenges/week1.js
+++ challenges/week1.js
@@ -34,11 +34,12 @@ function getSalePrice(originalPrice, reduction) {
 }
 
 function getMiddleCharacter(str) {
-  if (str === undefined) throw new Error("str is required");
+  if (str === undefined) {
+    throw new Error("str is required");}
   // Add your code here!
-
-
+  return str.length % 2 ? str.substr(str.length / 2, 1) : str.substr((str.length / 2) - 1, 2);
 }
+
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
